7937491aff0f8652213e336f2c6f94d075fadf47
qui2dty <96517530+qui2dty@users.noreply.github.com>
2024-08-25 16:31:53 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,20 @@
+const solution = function (string) {
+  let arr = string.split("");
+  let SlicedArr;
+  let answer = 0;
+  if (arr.length % 2 !== 0) {
+    SlicedArr = arr.slice(
+      Math.floor(arr.length / 2),
+      Math.floor(arr.length / 2) + 1
+    );
+
+    //console.log(Math.floor(arr.length / 2 + 1));
+  } else {
+    SlicedArr = arr.slice(
+      Math.floor(arr.length / 2) - 1,
+      Math.floor(arr.length / 2) + 1
+    );
+  }
+  answer = SlicedArr.join("");
+  return answer;
+};
