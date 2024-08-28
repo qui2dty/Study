54d7de9280fb65aa54b71834fd4de9edb8816147
qui2dty <96517530+qui2dty@users.noreply.github.com>
2024-08-28 12:28:21 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 15.47 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,11 @@
+let solution = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 50 && arr[i] % 2 == 0) {
+      arr[i] = arr[i] / 2;
+    } else if (arr[i] < 50 && arr[i] % 2 !== 0) {
+      arr[i] = arr[i] * 2;
+    }
+  }
+
+  return arr;
+};
