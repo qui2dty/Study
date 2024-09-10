36fecbbbeed4d3cb216b9509501e9062d5390426
qui2dty <96517530+qui2dty@users.noreply.github.com>
2024-09-10 12:34:00 +0900

    [level 1] Title: 시저 암호, Time: 3.00 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,24 @@
+const solution = function (s, n) {
+  let answer = "";
+  let sString;
+
+  for (let i = 0; i < s.length; i++) {
+    let sASCII = s.charCodeAt(i);
+
+    if (sASCII >= 65 && sASCII <= 90) {
+      sASCII += n;
+      if (sASCII > 90) {
+        sASCII -= 26;
+      }
+    } else if (sASCII >= 97 && sASCII <= 122) {
+      sASCII += n;
+      if (sASCII > 122) {
+        sASCII -= 26;
+      }
+    }
+    sString = String.fromCharCode(sASCII);
+
+    answer += sString;
+  }
+  return answer;
+};
